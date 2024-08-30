77e533ce200486c705899ea12712bca9fa7f1614
divyansh7705 <divyanshrastogi91@gmail.com>
2024-08-30 23:45:04 +0530

    Add files via upload

---
+++ concatenated.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string a,b;
+    cout<<"Enter first sring: "<<endl;
+    cin>>a;
+    cout<<"Enter second sring: "<<endl;
+    cin>>b;
+    cout<<"Concatenated String is: "<<a+b<<endl;
+}
